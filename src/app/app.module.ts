fd00b421b88642ae224aeca95392a9e530828601
emu <bsse0927@iit.du.ac.bd>
2020-02-13 16:33:31 +0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    CreateEmployeeComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/create-employee/create-employee.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
